494a08b9380c77301e90c9e96120b5a7a36fa107
Jason Balaci <jasonbalaci@gmail.com>
2022-02-14 15:18:54 -0500

    Clean up type signatures

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE TypeApplications #-}
--- TODO: NoMonomorphismRestriction is a bit of a double-sided sword. We _should not_ leave Chunks with polymorphic type parameters because references should to it should all be able to assume the same monomorphic type. Surprisingly, it's good to have this flag disabled! But I like it, and I'm aware of it, so I'll keep it enabled...
+{- TODO: NoMonomorphismRestriction is a bit of a double-edged sword. We _should
+not_ leave Chunks with polymorphic type parameters because references should to
+it  should all be able to assume the same monomorphic type. Surprisingly, it's
+good to have this flag disabled! But I like it, and I'm aware of it, so I'll keep
+it enabled... -}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 module Lib where
@@ -9,7 +13,7 @@ import Knowledge.Concepts.Definition (Definition, mkDefinition)
 import Knowledge.Maths.Expr (Expr, ExprC (..))
 import Knowledge.Maths.Literal (LiteralC (..))
 import Knowledge.Maths.QuantityDict (QuantityDict, mkQuantityDict)
-import Knowledge.Maths.QuantityDict.Definition (mkQDefinition)
+import Knowledge.Maths.QuantityDict.Definition (QDefinition, mkQDefinition)
 import KnowledgeBase.ChunkDB (ChunkDB, empty, insert')
 import KnowledgeBase.TypedUIDRef (mkRef)
 import KnowledgeBase.UID (mkUid)
@@ -26,19 +30,21 @@ func1 = mkQuantityDict (Proxy @(Int -> Int)) (mkUid "func1") "c1" "d1"
 func2 :: QuantityDict (Bool -> Int -> Int -> Int)
 func2 = mkQuantityDict (Proxy @(Bool -> Int -> Int -> Int)) (mkUid "func2") "c2" "d2"
 
-varDef1 :: Definition (QuantityDict Int) (Expr Int)
+varDef1 :: QDefinition Expr Int
 varDef1 = mkQDefinition (mkUid "varDef1") var1 (int 1) "e1" "f1"
 
-varDef2 :: Definition (QuantityDict Bool) (Expr Bool)
+varDef2 :: QDefinition Expr Bool
 varDef2 = mkQDefinition (mkUid "varDef2") var2 (not_ $ bool False) "e2" "f2"
 
-funcDef1 :: Definition (QuantityDict (Int -> Int)) (Expr (Int -> Int))
+funcDef1 :: QDefinition Expr (Int -> Int)
 funcDef1 = mkQDefinition (mkUid "funcDef1") func1 (lam $ \x -> add x (int 1)) "g1" "h1"
 
-funcDef2 :: Definition (QuantityDict (Bool -> Int -> Int -> Int)) (Expr (Bool -> Int -> Int -> Int))
+{- NOTE: Without the `QDefinition` type alias, we need the following type signature:
+funcDef2 :: Definition (QuantityDict (Bool -> Int -> Int -> Int)) (Expr (Bool -> Int -> Int -> Int)) -}
+funcDef2 :: QDefinition Expr (Bool -> Int -> Int -> Int)
 funcDef2 = mkQDefinition (mkUid "funcDef2") func2 (lam $ \b -> lam $ \x -> lam $ \y -> ifTE b x y) "g2" "h2"
 
-funcDef3 :: Definition (QuantityDict (Int -> Int)) (Expr (Int -> Int))
+funcDef3 :: QDefinition Expr (Int -> Int)
 funcDef3 = mkQDefinition (mkUid "funcDef3") func1 (lam (\x -> add x (int 1))) "g3" "h3"
 
 {-
@@ -54,11 +60,14 @@ funcDef4 :: ExprC r =>
 funcDef4 = mkDefinition (mkUid "funcDef4") func2 (lam (\b -> lam (lam . ifTE b))) "g4" "gh"
 
 Luckily, we can just create a helper function for things that we commonly define with a type param
-to straighten up the type nicely: e.g., `mkQDefinition` (QuantityDict Definition)
+to straighten up the type nicely: e.g., `mkQDefinition` (QuantityDict Definition).
+
+This, in conjunction with the QDefinition type alias, allows HLS to infer "QDefinition"
+type signatures fairly easily!
 
 -}
 
-funcDef4 :: Definition (QuantityDict (Bool -> Int -> Int -> Int)) (Expr (Bool -> Int -> Int -> Int))
+funcDef4 :: QDefinition Expr (Bool -> Int -> Int -> Int)
 funcDef4 = mkQDefinition (mkUid "funcDef4") func2 (lam (\b -> lam (lam . ifTE b))) "g4" "gh"
 
 cdb :: ChunkDB
